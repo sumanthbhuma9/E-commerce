b3f41e061ec01d74420b042f9eb546306cca1fb1
sumanthbhuma9 <bhumasumanth2006@gmail.com>
2026-07-15 10:53:52 +0530

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/E-commerce">
   <React.StrictMode>
     <App />
   </React.StrictMode>,
